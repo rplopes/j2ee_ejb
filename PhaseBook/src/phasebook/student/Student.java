f32e3f253678aeaee9cbd3c9611f0d184658f632
unknown <Gustavo@.(none)>
2011-10-17 17:51:31 +0100

    Criacao das classes friendship e lottery

--- PhaseBook/src/phasebook/student/Student.java
+++ PhaseBook/src/phasebook/student/Student.java
@@ -4,9 +4,10 @@ import java.io.Serializable;
 import javax.persistence.*;
 
 @Entity
-public class Student implements Serializable {
+public class Student implements Serializable 
+{
 
-private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
