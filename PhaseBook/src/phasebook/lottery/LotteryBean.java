c5d29f1307bcbeb7e2b37092124832d741041ec7
Ricardo Lopes <mail@ricardolopes.net>
2011-10-25 03:04:48 +0100

    Adicionado 3o project, para a external library, e feita a integração da lottery do phasebook com a lottery do phasebooklottery

--- PhaseBook/src/phasebook/lottery/LotteryBean.java
+++ PhaseBook/src/phasebook/lottery/LotteryBean.java
@@ -2,23 +2,46 @@ package phasebook.lottery;
 
 import java.text.*;
 import java.util.Calendar;
+import java.util.Date;
 
-import javax.ejb.Stateless;
+import javax.annotation.Resource;
+import javax.ejb.*;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import phasebook.external.lottery.ExternalLotteryRemote;
 
 @Stateless
 public class LotteryBean implements LotteryRemote {
 	
-	public String draw() {
-		DateFormat dateFormat = new SimpleDateFormat("HH:mm - dd/MM/yyyy");
-        Calendar draw = Calendar.getInstance();
-        return "Hello from control: " + dateFormat.format(draw.getTime());
-    }
+	public static int number = -1;
+	public static String nextDraw = "";
+	public static int timerInterval = 1000 * 60;
+	private @Resource SessionContext ctx;
+	  
+	public void scheduleTimer(long milliseconds) {  
+		ctx.getTimerService().createTimer(new Date(new Date().getTime() + milliseconds), "Hello World");  
+	}
 	
 	public String nextDrawDate() {
 		DateFormat dateFormat = new SimpleDateFormat("HH:mm - dd/MM/yyyy");
 		Calendar nextDraw = Calendar.getInstance();
 		nextDraw.add(Calendar.MINUTE, 1);
 		return dateFormat.format(nextDraw.getTime());
+	}
+	
+	@Timeout
+	public void timeout(Timer timer){
+		try {
+			InitialContext ctx = new InitialContext();
+			ExternalLotteryRemote lottery = (ExternalLotteryRemote) ctx.lookup("ExternalLotteryBean/remote");
+			number = lottery.getNumber();
+			nextDraw = lottery.getNextDraw();
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Phasebook - " + number + ". Next at " + nextDraw);
+		this.scheduleTimer(timerInterval);
 	}
 
 }
---
+++ PhaseBook/src/phasebook/lottery/LotteryMain.java
@@ -0,0 +1,23 @@
+package phasebook.lottery;
+
+import java.util.Hashtable;
+import javax.naming.InitialContext;
+
+public class LotteryMain {
+	
+	/*
+	 * Should only be run to start the internal lottery service if it's not already running
+	 */
+	public static void main(String[] args) throws Exception {  
+		Hashtable ht = new Hashtable();
+		ht.put(InitialContext.INITIAL_CONTEXT_FACTORY,"org.jnp.interfaces.NamingContextFactory");
+		ht.put(InitialContext.PROVIDER_URL,"jnp://localhost:1099");
+		ht.put(InitialContext.URL_PKG_PREFIXES,"org.jboss.naming:org.jnp.interfaces");
+		
+		InitialContext ctx = new InitialContext(ht);
+		
+		LotteryRemote timer = (LotteryRemote) ctx.lookup("LotteryBean/remote");
+		timer.scheduleTimer(1);
+	}
+
+}
