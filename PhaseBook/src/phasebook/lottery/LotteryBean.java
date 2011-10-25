e06fe04fc57de2b8d532200ff0edde961779fcc6
Ricardo Lopes <mail@ricardolopes.net>
2011-10-25 03:40:57 +0100

    já cria entradas na tabela Lottery

--- PhaseBook/src/phasebook/lottery/LotteryBean.java
+++ PhaseBook/src/phasebook/lottery/LotteryBean.java
@@ -1,13 +1,15 @@
 package phasebook.lottery;
 
-import java.text.*;
-import java.util.Calendar;
 import java.util.Date;
 
 import javax.annotation.Resource;
 import javax.ejb.*;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
 import phasebook.external.lottery.ExternalLotteryRemote;
 
@@ -24,10 +26,7 @@ public class LotteryBean implements LotteryRemote {
 	}
 	
 	public String nextDrawDate() {
-		DateFormat dateFormat = new SimpleDateFormat("HH:mm - dd/MM/yyyy");
-		Calendar nextDraw = Calendar.getInstance();
-		nextDraw.add(Calendar.MINUTE, 1);
-		return dateFormat.format(nextDraw.getTime());
+		return nextDraw;
 	}
 	
 	@Timeout
@@ -37,11 +36,26 @@ public class LotteryBean implements LotteryRemote {
 			ExternalLotteryRemote lottery = (ExternalLotteryRemote) ctx.lookup("ExternalLotteryBean/remote");
 			number = lottery.getNumber();
 			nextDraw = lottery.getNextDraw();
+			if (number > 0)
+				createDraw(number, new Date());
 		} catch (NamingException e) {
 			e.printStackTrace();
 		}
 		System.out.println("Phasebook - " + number + ". Next at " + nextDraw);
 		this.scheduleTimer(timerInterval);
+	}
+	
+	private void createDraw(int number, Date date) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("PhaseBook");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		
+		tx.begin();
+    	Lottery lottery = new Lottery();
+    	lottery.setLotteryNumber(number);
+    	lottery.setLotteryDate(new java.sql.Date(date.getTime()));
+		em.persist(lottery);
+		tx.commit();
 	}
 
 }
