f7932c5036bde476894778bb22a9df1aef6d5944
Ricardo Lopes <mail@ricardolopes.net>
2011-10-26 16:50:16 +0100

    resolvidos problemas do rebase

--- PhaseBook/src/phasebook/lottery/Lottery_.java
+++ PhaseBook/src/phasebook/lottery/Lottery_.java
@@ -1,17 +1,17 @@
 package phasebook.lottery;
 
-import java.sql.Date;
+import java.sql.Timestamp;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import phasebook.lotterybet.LotteryBet;
 
-@Generated(value="Dali", date="2011-10-26T16:35:52.017+0100")
+@Generated(value="Dali", date="2011-10-26T16:36:48.035+0100")
 @StaticMetamodel(Lottery.class)
 public class Lottery_ {
 	public static volatile SingularAttribute<Lottery, Integer> id;
 	public static volatile SingularAttribute<Lottery, Integer> lotteryNumber;
-	public static volatile SingularAttribute<Lottery, Date> lotteryDate;
+	public static volatile SingularAttribute<Lottery, Timestamp> lotteryDate;
 	public static volatile ListAttribute<Lottery, LotteryBet> lotteryBets;
 }
--- PhaseBook/src/phasebook/lotterybet/LotteryBet_.java
+++ PhaseBook/src/phasebook/lotterybet/LotteryBet_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 import phasebook.lottery.Lottery;
 import phasebook.user.PhasebookUser;
 
-@Generated(value="Dali", date="2011-10-26T16:36:02.337+0100")
+@Generated(value="Dali", date="2011-10-26T16:36:48.126+0100")
 @StaticMetamodel(LotteryBet.class)
 public class LotteryBet_ {
 	public static volatile SingularAttribute<LotteryBet, Integer> id;
@@ -16,5 +16,5 @@ public class LotteryBet_ {
 	public static volatile SingularAttribute<LotteryBet, Integer> betNumber;
 	public static volatile SingularAttribute<LotteryBet, Date> createdAT;
 	public static volatile SingularAttribute<LotteryBet, Lottery> lottery;
-	public static volatile SingularAttribute<LotteryBet, Integer> valueWon;
+	public static volatile SingularAttribute<LotteryBet, Float> valueWon;
 }
