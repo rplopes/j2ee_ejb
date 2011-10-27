20e632b767598df4826cc0637c2af943df7d8377
Rui Chicória <rchicoria@gmail.com>
2011-10-27 15:45:17 +0100

    mail de post a funcionar

--- PhaseBook/src/phasebook/user/PhasebookUserBean.java
+++ PhaseBook/src/phasebook/user/PhasebookUserBean.java
@@ -3,10 +3,18 @@ package phasebook.user;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.ejb.Stateless;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -26,6 +34,14 @@ import phasebook.post.Post;
  */
 @Stateless
 public class PhasebookUserBean implements PhasebookUserRemote {
+	
+	public static String MAIN_PATH   = "http://localhost:8080/photos/";
+	public static int IMG_DEFAULT_WIDTH = 300; 
+	
+	private static final String SMTP_HOST_NAME = "smtp.gmail.com";
+    private static final int SMTP_HOST_PORT = 465;
+    private static final String SMTP_AUTH_USER = "phasebookmessager@gmail.com";
+    private static final String SMTP_AUTH_PWD  = "grrgrrgrr";
 
     /**
      * Default constructor. 
@@ -186,13 +202,13 @@ public class PhasebookUserBean implements PhasebookUserRemote {
 		EntityTransaction tx = em.getTransaction();
 		
 		tx.begin();
-		System.out.println("\n\n\n\n\n"+privacy+"\n\n\n\n");
     	Post post = new Post(from, to, text, privacy);
 		em.persist(post);
 		em.refresh(post);
 		tx.commit();
 		em.close();
 		emf.close();
+		notifyUser(to, "PHASEBOOK: You have a new post", from.getName()+" posted a message on your wall:<br><br>\""+text+"\"<br><br>"+a("Go to your wall"));
 	}
 	
 	public void addPost(PhasebookUser from, PhasebookUser to, String text, String photoLink, String privacy){
@@ -212,6 +228,7 @@ public class PhasebookUserBean implements PhasebookUserRemote {
 		tx.commit();
 		em.close();
 		emf.close();
+		notifyUser(to, "PHASEBOOK: You have a new post", from.getName()+" posted a message on your wall:<br><br>"+img(photo.getName(), from.getId())+"<br><br>\""+text+"\"<br><br>"+a("Go to your wall"));
 	}
 	
 	public Photo addPhoto(String photoLink){
@@ -243,6 +260,7 @@ public class PhasebookUserBean implements PhasebookUserRemote {
 		tx.commit();
 		em.close();
 		emf.close();
+		notifyUser(hostUser, "PHASEBOOK: "+hostUser.getName()+" invited you", invitedUser.getName()+" invited you to be his friend<br><br>"+a("Go to your notifications"));
 	}
 	
 	public void setProfilePicture(PhasebookUser user, Photo photo)
@@ -339,6 +357,58 @@ public class PhasebookUserBean implements PhasebookUserRemote {
 		d.reset();
 		d.update(x.getBytes());
 		return d.digest();
+	}
+	
+	public void notifyUser(PhasebookUser user, String subject, String body){
+		Properties props = new Properties();
+
+        props.put("mail.transport.protocol", "smtps");
+        props.put("mail.smtps.host", SMTP_HOST_NAME);
+        props.put("mail.smtps.auth", "true");
+        // props.put("mail.smtps.quitwait", "false");
+
+        Session mailSession = Session.getDefaultInstance(props);
+        mailSession.setDebug(true);
+        Transport transport = null;
+		try {
+			transport = mailSession.getTransport();
+		} catch (NoSuchProviderException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        MimeMessage message = new MimeMessage(mailSession);
+        try {
+			message.setSubject(subject);
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+    		message.setContent(body, "text/html");
+			message.addRecipient(Message.RecipientType.TO,
+		             new InternetAddress(user.getEmail()));
+			transport.connect
+	          (SMTP_HOST_NAME, SMTP_HOST_PORT, SMTP_AUTH_USER, SMTP_AUTH_PWD);
+
+	        transport.sendMessage(message,
+	            message.getRecipients(Message.RecipientType.TO));
+	        transport.close();
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	public static String img(String url, int id)
+	{
+		return "<img src=\""+MAIN_PATH+"/"+id+"/"+url+"\" width=\""+IMG_DEFAULT_WIDTH+"\" />";
+	}
+	
+	public static String a(String text)
+	{
+		return "<a href='http://localhost:8080/PhaseBookWeb'>" + text + "</a>";
 	}
 	
 }
